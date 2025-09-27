0496c03ad878cb5687020d12ad0eb25eca8b567e
dickxumamba <xuruoyu27@gmail.com>
2025-09-27 19:48:36 +0800

    updated

--- lab15.cpp
+++ lab15.cpp
@@ -5,6 +5,20 @@
 #include <fstream>
 using namespace std;
 
+class Movie {
+    private:
+    string screen_writer;
+    int year_released;
+    string title;
+
+    public:
+    // setters and getters
+    void set_screen_writer(){}
+    string get_screen_writer(){}
+
+    
+};
+
 int main() {
     
     

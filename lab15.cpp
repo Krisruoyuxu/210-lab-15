b93456e8c0812d0d9d5a15b664bfb49d625f4b79
dickxumamba <xuruoyu27@gmail.com>
2025-09-28 16:27:42 +0800

    updated, next, trying to read the external file and create objects by use it

--- lab15.cpp
+++ lab15.cpp
@@ -14,27 +14,40 @@ class Movie {
     public:
     // setters and getters
     void set_screen_writer(string input){ screen_writer = input; }
-    string get_screen_writer(){ return screen_writer; }
+    string get_screen_writer()const { return screen_writer; }
 
     void set_year_released(int input){ year_released = input; }
-    int get_year_released(){ return year_released; }
+    int get_year_released()const { return year_released; }
 
     void set_title(string input){ title = input; }
-    string get_title(){ return title; }
+    string get_title()const { return title; }
 
     void print(){
-        cout << "Movie: " << title <<endl;
+        cout << "Movie: " << screen_writer <<endl;
         cout << "    Year released: " << year_released <<endl;
-        cout << "    Screenwriter: " << screen_writer <<endl;
+        cout << "    Screenwriter: " << title <<endl;
+        cout <<endl;
     }
 
 };
 
 int main() {
     
-    Movie movie1;
+    Movie movie1; // just a test now
+    Movie movie2; 
     movie1.set_screen_writer("TestScreenWriter1");
-    movie1.
+    movie1.set_year_released(1000);
+    movie1.set_title("hello");
+
+    movie2.set_screen_writer("TestScreenWriter2");
+    movie2.set_year_released(2000);
+    movie2.set_title("asdf");
+
+    movie2.print();
+
+    movie1.print();
+
+
 
     return 0;
 }
